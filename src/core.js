60e2458b8332a4825d2008dc37372f40bc19ab73
Duncan Hall <himself@duncanhall.net>
2016-10-05 19:18:46 +0100

    Single screen-bounded node

--- src/core.js
+++ src/core.js
@@ -1,9 +1,13 @@
 'use strict';
 
-const renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, {antialias:true});
+let screenWidth = window.innerWidth;
+let screenHeight = window.innerHeight;
+
+const renderer = PIXI.autoDetectRenderer(screenWidth, screenHeight, {antialias:true});
 const stage = new PIXI.Container();
 const graphics = new PIXI.Graphics();
-const circles = [];
+const nodes = [];
+const randSpeed = (a,b) => parseFloat((((a - b) * Math.random()) + b).toFixed(1));
 
 renderer.backgroundColor = 0x878787;
 renderer.autoResize = true;
@@ -11,26 +15,48 @@ renderer.view.style.position = "absolute";
 renderer.view.style.display = "block";
 
 document.body.appendChild(renderer.view);
+window.onresize = resizeHandler;
 
 stage.addChild(graphics);
-createCircle();
+createNode();
 animate();
 
+function resizeHandler () {
+  screenWidth = window.innerWidth;
+  screenHeight = window.innerHeight;
+  renderer.resize(screenWidth, screenHeight);
+}
+
 function animate() {
-  circles.forEach(drawCircle);
+  update();
+  redraw();
   requestAnimationFrame(animate);
   renderer.render(stage);
 }
 
-function createCircle() {
-  circles.push({
+function createNode() {
+  nodes.push({
     x: Math.random() * window.innerWidth,
-    y: Math.random() * window.innerHeight
+    y: Math.random() * window.innerHeight,
+    vx: randSpeed(-2.5, 2.5),
+    vy: randSpeed(-2.5, 2.5)
   });
 }
 
-function drawCircle(c) {
-  graphics.beginFill(0xFFFFFF, 1);
-  graphics.drawCircle(c.x, c.y, 5);
-  graphics.endFill();
+function update() {
+  nodes.forEach(n => {
+    n.x += n.vx; 
+    n.y += n.vy
+    if (n.x < 0 || n.x > screenWidth) n.vx *= -1;
+    if (n.y < 0 || n.y > screenHeight) n.vy *= -1; 
+  });
+}
+
+function redraw(c) {
+  graphics.clear();
+  nodes.forEach(n => {
+    graphics.beginFill(0xFFFFFF, 1);
+    graphics.drawCircle(n.x, n.y, 5);
+    graphics.endFill();
+  })
 }
